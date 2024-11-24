65bf7b528b3895900055648553418563593d0d6e
aminjlassi1 <mihamedamin.jlassi@sesame.com.tn>
2024-11-24 16:51:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  isModalOpen = false; // To toggle modal visibility
+  client = { username: '', email: '', password: '' }; // For form data
+  inscriptions: any[] = []; // List of registered clients
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.resetForm();
+  }
+
+  submitForm(form: any) {
+    if (form.valid) {
+      this.inscriptions.push({ ...this.client });
+      this.resetForm();
+      this.closeModal();
+    }
+  }
+
+  resetForm() {
+    this.client = { username: '', email: '', password: '' };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Ajoutez cette ligne
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
